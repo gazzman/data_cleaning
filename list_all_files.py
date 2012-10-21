d28ddd7dc47385e5cce15e4115f348114efb42b8
gazzman <sgazzisg@uw>
2012-10-21 13:56:02 -0400

    Initial add of the list all files utilty.

---
+++ list_all_files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import argparse
+import glob
+
+def print_files(t, windows=False, s='/*'):
+    if t[-1] == '/': t = t[:-1]
+    count = 1
+    path = t + count*s
+    f = glob.glob(path)
+    while len(f) > 0:
+        filepaths = '\n'.join(f)
+        if windows: print filepaths.replace('/', '\\')
+        else: print filepaths
+        count += 1
+        path = t + count*s
+        f = glob.glob(path)
+
+# For running from command line
+if __name__ == "__main__":
+    description = 'Print paths to all subfolders and files.'
+
+    a = argparse.ArgumentParser(description=description)
+    a.add_argument('-p', type=str, default='./', dest='top_level', 
+                   help='The top-level path. Default is \'./\'.')
+    a.add_argument('-w', dest='windows', action='store_true',
+                   help='If set, changes forwardslashes to backslashes.')
+
+    args = a.parse_args()
+    print_files(args.top_level, args.windows)
