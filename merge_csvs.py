bc0d4f371b49e80a7ec47e6c1e3ea9fa81369109
gazzman <sgazzisg@uw>
2012-12-23 22:54:49 -0500

    Improved functionality to merge any number of csv files into one.

--- merge_csvs.py
+++ merge_csvs.py
@@ -7,36 +7,44 @@ __author__ = "gazzman"
 __copyright__ = "(C) 2012 gazzman GNU GPL 3."
 __contributors__ = []
 
-def merge_csvs(first_csv, second_csv, merged_csv):
-    csv1 = csv.DictReader(open(first_csv, 'r'))
-    csv2 = csv.DictReader(open(second_csv, 'r'))
+def merge_csvs(filenames, merged_csv):
+    csvs = []
+    headers = []
+    for filename in filenames:
+        csv_file = csv.DictReader(open(filename, 'r'))
+        csvs += [csv_file]
+        headers += [x for x in csvs[-1].fieldnames if not x in headers]
 
-    headers = csv1.fieldnames
-    headers += [x for x in csv2.fieldnames if not x in headers]
     with open(merged_csv, 'w') as f:
         f.write(','.join(headers) + '\n')
         mcsv = csv.DictWriter(f, headers)
-        for row in csv1: mcsv.writerow(row)
-        for row in csv2: mcsv.writerow(row)
+        for csv_file in csvs:
+            for row in csv_file:
+                mcsv.writerow(row)
 
 # For running from command line
 if __name__ == "__main__":
-    description = 'Merge two csv files into one.'
-    description += ' Both csvs must have headers as their first rows.'
+    description = 'Merge several csv files into one.'
+    description += ' All csvs must have headers as their first rows.'
 
     merged_csv = 'merged.csv'
     help = 'name of merged csv file.'
     help += ' Defaults to \'' + merged_csv + '\'.'
 
     p = argparse.ArgumentParser(description=description)
-    p.add_argument('first_csv', type=str, 
-                   help='the first csv file.')
-    p.add_argument('second_csv', type=str, 
-                   help='the second csv file.')
+    p.add_argument('csv_list_file', type=str, 
+                   help='a text file containing the list of csvs to merge.')
     p.add_argument('-s', metavar='merged_csv', default=merged_csv, dest='merged_csv', 
                     help=help)
     p.add_argument('-v', '--version', action='version', 
                    version='%(prog)s ' + __version__)
 
     args = p.parse_args()
-    merge_csvs(args.first_csv, args.second_csv, args.merged_csv)
+
+    filenames = []
+    with open(args.csv_list_file) as f:
+        for line in f:
+            filename = line.strip()
+            if filename[0] != '#': filenames += [filename]
+
+    merge_csvs(filenames, args.merged_csv)
